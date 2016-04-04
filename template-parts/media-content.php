f91b5ec17927f56d4c32adfdff8d794fbda4fdd4
Partin <partin.imeri@gmail.com>
2016-04-04 11:31:08 +0200

    Added message when no posts avaliable.

--- template-parts/media-content.php
+++ template-parts/media-content.php
@@ -18,45 +18,52 @@
 
                  $categories = get_the_category();
                  $cat_id = $categories[0]->cat_ID;
-                 $args = array(
-                         'post_type' => 'post',
-                         'posts_per_page' => $postsPerPage,
-                         'cat'=>$cat_id
-                 );
+                 if($cat_id != ""){
 
-                 $loop = new WP_Query($args);
-                 while ($loop->have_posts()) : $loop->the_post();
-                 $featured_image_url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
-                 if($featured_image_url != ""){ ?>
-                   <div class="col-xs-12 col-lg-3 item <?php foreach(get_the_category() as $category) { echo $category->slug . '';} ?>">
-                     <a href="<?php the_permalink()?>" class="article-full-img">
-                       <div class="article-img" style="background-image: url('<?php echo $featured_image_url ?>')"></div>
-                       <div class="article">
-                         <div class="category"><?php foreach(get_the_category() as $category) { echo $category->cat_name;} ?></div>
-                         <div class="date"><?php echo get_the_date();?></div>
-                         <h3><?php the_title(); ?></h3>
-                         <div class="read-more">Read More <span class="icon-arrow-right"></span></div>
-                       </div>
-                     </a>
-                   </div>
 
-                 <?php }else{ ?>
+                       $args = array(
+                               'post_type' => 'post',
+                               'posts_per_page' => $postsPerPage,
+                               'cat'=>$cat_id
+                       );
 
-                     <div class="col-xs-12 col-lg-3 item <?php foreach(get_the_category() as $category) { echo $category->slug . '';} ?>">
-                       <a href="<?php  the_permalink(); ?>" >
-                         <div class="article">
-                           <div class="category"><?php foreach(get_the_category() as $category) { echo $category->cat_name;} ?></div>
-                           <div class="date"><?php echo get_the_date();?></div>
-                           <h3><?php the_title(); ?></h3>
-                           <div class="read-more">Read More <span class="icon-arrow-right"></span></div>
+                       $loop = new WP_Query($args);
+                       while ($loop->have_posts()) : $loop->the_post();
+                       $featured_image_url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
+                       if($featured_image_url != ""){ ?>
+                         <div class="col-xs-12 col-lg-3 item <?php foreach(get_the_category() as $category) { echo $category->slug . '';} ?>">
+                           <a href="<?php the_permalink()?>" class="article-full-img">
+                             <div class="article-img" style="background-image: url('<?php echo $featured_image_url ?>')"></div>
+                             <div class="article">
+                               <div class="category"><?php foreach(get_the_category() as $category) { echo $category->cat_name;} ?></div>
+                               <div class="date"><?php echo get_the_date();?></div>
+                               <h3><?php the_title(); ?></h3>
+                               <div class="read-more">Read More <span class="icon-arrow-right"></span></div>
+                             </div>
+                           </a>
                          </div>
-                       </a>
-                     </div>
 
-                   <?php
-                     }
-                   endwhile;
-           wp_reset_postdata();
+                       <?php }else{ ?>
+
+                           <div class="col-xs-12 col-lg-3 item <?php foreach(get_the_category() as $category) { echo $category->slug . '';} ?>">
+                             <a href="<?php  the_permalink(); ?>" >
+                               <div class="article">
+                                 <div class="category"><?php foreach(get_the_category() as $category) { echo $category->cat_name;} ?></div>
+                                 <div class="date"><?php echo get_the_date();?></div>
+                                 <h3><?php the_title(); ?></h3>
+                                 <div class="read-more">Read More <span class="icon-arrow-right"></span></div>
+                               </div>
+                             </a>
+                           </div>
+
+                         <?php
+                           }
+                         endwhile;
+                         wp_reset_postdata();
+                  }else{ ?>
+                    <div> No posts in this page.</div>
+                  <?php }
+
             ?>
             <div class="sidebar sidebar-nav col-lg-2">
               <?php
