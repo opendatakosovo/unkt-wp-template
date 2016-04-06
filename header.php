40da8312118272a10375018655c9f87c1c58bcce
Vullkan92 <vullkan.halili@riinvest.net>
2016-04-06 17:10:42 +0200

    made the submenu list vertical.

--- header.php
+++ header.php
@@ -145,7 +145,7 @@
 
 		                <li class="item">
 		                    <a href="<?php echo $link; ?>" class="title"><?php echo $title; ?></a>
-		                </li>
+		                </li><br>
 
 		            <?php if ( $menuitems[ $count + 1 ]->menu_item_parent != $parent_id && $submenu ): ?>
 		            </ul>
