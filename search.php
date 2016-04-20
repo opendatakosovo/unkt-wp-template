b04cdc739c13a5d2f6e4372b6ecc3e0fff681129
Partin <partin.imeri@gmail.com>
2016-04-20 14:33:58 +0200

    Fixed category not showing.

--- search.php
+++ search.php
@@ -30,14 +30,29 @@ $search = new WP_Query($search_query);
             $featured_image_url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
             $outside_link =get_field('external_source_link');
 
+            $the_category_slug = "";
+            $the_category = "";
+
+              if(get_post_type()=="post"){
+                foreach(get_the_category() as $category) {
+                  $the_category_slug = $category->slug . '';
+                  $the_category = $category->cat_name . '';
+                }
+              }
+              else {
+                $the_category_slug = 'events';
+                $the_category = 'Events';
+                $terms = get_the_terms(strval( $post->ID ),'ecwd_event_category');
+                $event_categories = wp_get_post_terms($post->ID, 'ecwd_event_category','');
+              }
                ?>
 
            <?php if($outside_link == ""){ ?>
-             <div class="col-xs-12 col-lg-3 item <?php foreach(get_the_category() as $category) { echo $category->slug . '';} ?>">
+             <div class="col-xs-12 col-lg-3 item <?php echo $the_category_slug; ?>">
                <a href="<?php  the_permalink(); ?>" class="article-full-img">
                  <div class="article-img" style="background-image: url('<?php echo $featured_image_url ?>')"></div>
                  <div class="article">
-                   <div class="category"><?php foreach(get_the_category() as $category) { echo $category->cat_name;} ?></div>
+                   <div class="category"><?php echo $the_category; ?></div>
                    <div class="date"><?php echo get_the_date('j M Y');?></div>
                    <h3><?php the_title(); ?></h3>
                    <div class="read-more" >Read More <span class="icon-arrow-right"></span></div>
@@ -45,11 +60,11 @@ $search = new WP_Query($search_query);
                </a>
              </div>
              <?php }else{ ?>
-               <div class="col-xs-12 col-lg-3 item <?php foreach(get_the_category() as $category) { echo $category->slug . '';} ?>">
+               <div class="col-xs-12 col-lg-3 item <?php echo $the_category_slug; ?>">
                  <a href="<?php  echo $outside_link; ?>" target="_blank" class="article-full-img">
                    <div class="article-img" style="background-image: url('<?php echo $featured_image_url ?>')"></div>
                    <div class="article">
-                     <div class="category"><?php foreach(get_the_category() as $category) { echo $category->cat_name;} ?></div>
+                     <div class="category"><?php echo $the_category; ?></div>
                      <div class="date"><?php echo get_the_date('j M Y');?></div>
                      <h3><?php the_title(); ?></h3>
                      <div class="read-more" >Read More <span class="icon-arrow-right"></span></div>
