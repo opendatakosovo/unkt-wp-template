bf550a805982ad008cf46aafbef5303e7d72ca00
Partin <partin.imeri@gmail.com>
2016-05-24 13:03:57 +0200

    Minor fix.

--- category-work-with-us.php
+++ category-work-with-us.php
@@ -119,7 +119,7 @@ $(function() {
                  <div class="load-more-home disable-button-no-more-posts-available col-xs-12 btn" data-exclude-posts="<?php echo implode(', ', $excluded_posts); ?>" data-grid="3" data-page-name="home" data-posts-per-page="<?php echo $postsPerPage ?>">No more posts available</div>
 
                 <?php else: ?>
-                  <div class="load-more-home col-xs-12 btn" data-category="<?php echo implode(', ', $in_category); ?>" data-exclude-posts="<?php echo implode(', ', $excluded_posts); ?>" data-grid="3" data-page-name="home" data-posts-per-page="<?php echo $postsPerPage ?>">Load more posts</div>
+                  <div class="load-more-home col-xs-12 btn" data-category="<?php echo $cat_id ?>" data-exclude-posts="<?php echo implode(', ', $excluded_posts); ?>" data-grid="3" data-page-name="home" data-posts-per-page="<?php echo $postsPerPage ?>">Load more posts</div>
                 <?php endif; ?>
           </div>
         </div>
