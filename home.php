cd3fad158fb7755c4b6fe219c35bfe832870e679
Partin <partin.imeri@gmail.com>
2016-09-30 13:06:05 +0200

    Replaced CDN script/styles links with local ones.

--- home.php
+++ home.php
@@ -150,7 +150,6 @@ $(".menu li").mouseleave(function(e) {
   	</div> -->
     <!-- <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css"> -->
     <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-    <link rel="stylesheet" href="/resources/demos/style.css">
     <script>
     $(function() {
       var tabs = $( "#tabs" ).tabs();
