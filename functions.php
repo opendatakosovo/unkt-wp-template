ce6310cc1facb180f6e1d2f53a2f068ec7cf9c4d
Partin <partin.imeri@gmail.com>
2017-03-20 16:31:59 +0100

    Removing wp emojis printing.

--- functions.php
+++ functions.php
@@ -3,6 +3,13 @@
 require_once ( get_stylesheet_directory() . '/theme-options.php' );
 add_theme_support( 'post-thumbnails' );
 
+// REMOVE WP EMOJI
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
+
 
 // add post-formats to post_type 'page'
 add_post_type_support( 'page', 'post-formats' );
